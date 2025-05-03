e1d83635b61aa4b857bd64268cd0bf70119b8caf
Rajkamal Yadav <143881829+Rajkamalout@users.noreply.github.com>
2025-05-03 19:48:13 +0530

    Revert "Pending changes exported from your codespace"

--- Projects/2-bharat-clock-version-one/src/assets/components/CurrentTime.jsx
+++ Projects/2-bharat-clock-version-one/src/assets/components/CurrentTime.jsx
@@ -2,6 +2,6 @@ let CurrentTime = () =>{
 
     let time = new Date();
 
-    return<p className="lead">This is the CurrentTime :{time.toLocaleDateString()} -{""}{time.toLocaleTimeString()}</p>;
+    return<p>This is the CurrentTime :{time.toLocaleDateString()} -{""}{time.toLocaleTimeString()}</p>;
 };
 export default CurrentTime
