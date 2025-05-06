b8ada1645fe3b70a445506337705bc3e0e86cb43
Rajkamal Yadav <143881829+Rajkamalout@users.noreply.github.com>
2025-05-06 15:16:27 +0000

    Initial commit

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,15 @@
+function AddTodo() {
+    return (
+        < div className="container text-center" >
+            <div className="row kg-row">
+                <div className="col-6 ">
+                    <input type="test" placeholder="Enter The Todo Here"></input>
+                </div>
+                <div className="col-4"><input type="date" /></div>
+                <div className="col-2"><button type="button" className="btn btn-success kg-button">Add</button></div>
+            </div>
+        </div >
+
+    );
+}
+export default AddTodo
---
+++ src/components/Todoitems.jsx
@@ -0,0 +1,11 @@
+import Todoitem from "./Todoitem"
+const TodoItems = ({ todoItems }) => {
+  return (
+    <div classNmae="items-container">
+      {todoItems.map(item =><Todoitem todate={item.dueDate} todoName={item.name}></Todoitem>)}
+      
+      
+    </div>
+  );
+};
+export default TodoItems 
