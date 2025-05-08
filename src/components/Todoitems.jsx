9d4f3b51d52aa3a927349446f76f5130b7e1f9d9
Rajkamal Yadav <143881829+Rajkamalout@users.noreply.github.com>
2025-05-08 11:24:54 +0000

    Your commit message here

--- src/components/Todoitems.jsx
+++ src/components/Todoitems.jsx
@@ -3,8 +3,6 @@ const TodoItems = ({ todoItems }) => {
   return (
     <div classNmae="items-container">
       {todoItems.map(item =><Todoitem todate={item.dueDate} todoName={item.name}></Todoitem>)}
-      
-      
     </div>
   );
 };
